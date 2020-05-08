1a4c3c835d5129b226fbefc4f1baf9749032af78
Pawel <pawelp5555@gmail.com>
2020-05-08 10:01:08 +0200

    first commit

---
+++ src/main/java/com/best/filechecker/FilecheckerApplication.java
@@ -0,0 +1,13 @@
+package com.best.filechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilecheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilecheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/best/filechecker/FilecheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best.filechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilecheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
