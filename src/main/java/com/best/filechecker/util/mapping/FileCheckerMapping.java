48e7c2ca876a61e1465a7c878ab9113e49cde837
Pawel <pawelp5555@gmail.com>
2020-05-08 11:38:10 +0200

    Add FileNames class for mapping

--- src/main/java/com/best/filechecker/util/mapping/FileCheckerMapping.java
+++ src/main/java/com/best/filechecker/util/mapping/FileCheckerMapping.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class FileCheckerMapping {
-
     public static final String MAIN_PAGE = "/";
     public static final String COMPARE = "compare";
 }
--- src/main/java/com/best/filechecker/util/mapping/FileNames.java
+++ src/main/java/com/best/filechecker/util/mapping/FileNames.java
@@ -4,8 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class ViewMapping {
-    public static final String COMPARE_VIEW = "compare";
-    public static final String INDEX_VIEW = "index";
+public final class FileNames {
+    public static final String OUTPUT_FILE = "output";
 
 }
